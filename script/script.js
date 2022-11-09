7daf7dcbdcf5126b0b2e66320edb0e5a32a51998
V4r3us1n <98497693+V4r3us1n@users.noreply.github.com>
2022-11-09 14:08:33 -0300

    Tentativa nº¹ de tornar o iframe-conteudo responsivo
    Adicionei o evento DOM "onclick" nos itens do menu, no entanto estou tendo um problema com o item "projetos".
    Fiz uma mudança peculiar no item "projetos", onde os links variam de 1 a 2 colunas de acordo com o tamanho da tela, gostaria q o iframe mudasse também, mas somente quando a guia selecionada nele fosse do item "projetos", podento alterar seu tamanho para reponder à mudança de colunas nos links. Acho q deu para entender :T

--- script/script.js
+++ script/script.js
@@ -12,3 +12,22 @@ function mudarImagemCapa() {
 function mudarImagemPerfil() {
 
 }
+
+function mudarTamanhoIframe1() {
+    document.getElementById("iframe-conteudo").style.height = "420px";
+}
+
+function mudarTamanhoIframe2() {
+
+        if (window.innerWidth >= "1320") {
+            document.getElementById("iframe-conteudo").style.height = "800px";
+            console.log("a altura do iframe será 800px")
+        } else {
+            console.log("a altura do iframe será 1100px")
+            document.getElementById("iframe-conteudo").style.height = "1100px";
+        }
+}
+
+function mudarTamanhoIframe3() {
+    document.getElementById("iframe-conteudo").style.height = "700px";
+}
