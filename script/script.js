46e25033834d86ca18d60f139f1db5c38a541f78
V4r3us1n <98497693+V4r3us1n@users.noreply.github.com>
2022-11-15 12:33:19 -0300

    Adicionando Projetos dentro do Item Projetos e Adicionando mais um Conteúdo no Item Estudos

--- script/script.js
+++ script/script.js
@@ -13,21 +13,41 @@ function mudarImagemPerfil() {
 
 }
 
+let valoriframe = 1;
+
+let tamanho_tela = window.innerWidth;
+
 function mudarTamanhoIframe1() {
     document.getElementById("iframe-conteudo").style.height = "420px";
+    valoriframe = 1;
 }
 
 function mudarTamanhoIframe2() {
-
-        if (window.innerWidth >= "1320") {
-            document.getElementById("iframe-conteudo").style.height = "800px";
-            console.log("a altura do iframe será 800px")
-        } else {
-            console.log("a altura do iframe será 1100px")
-            document.getElementById("iframe-conteudo").style.height = "1100px";
-        }
+    if (window.innerWidth >= "1320") {
+        document.getElementById("iframe-conteudo").style.height = "800px";
+        console.log("a altura do iframe será 800px")
+    } else {
+        console.log("a altura do iframe será 1100px")
+        document.getElementById("iframe-conteudo").style.height = "1100px";
+    }
+    valoriframe = 2;
 }
 
-function mudarTamanhoIframe3() {
-    document.getElementById("iframe-conteudo").style.height = "700px";
+function mudarTamanhoIframe1() {
+    document.getElementById("iframe-conteudo").style.height = "550px";
+    valoriframe = 3;
+}
+
+function checarResponsividadeIframe() {
+    if (valoriframe ==  2) {
+        if (tamanho_tela != window.innerWidth) {
+            if (window.innerWidth >= "1320") {
+                document.getElementById("iframe-conteudo").style.height = "800px";
+                console.log("a altura do iframe será 800px")
+            } else {
+                console.log("a altura do iframe será 1100px")
+                document.getElementById("iframe-conteudo").style.height = "1100px";
+            }
+        }
+    }
 }
